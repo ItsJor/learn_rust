004204288f9755d293905134a3d5f98a815f3f52
Jordan Lin <jordanlin1360@gmail.com>
2026-09-25 01:18:11 -0400

    temp

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
    
     // program to ask a user to type a number in seconds and convert it to minutes, hours and seconds
     // will ask for another number, all until the user types q
+    // 9876 to 2 hours, 44 minutes and 36 seconds
 
     // constants: define seconds per minute and seconds per hour as constants with type annotation
     // write hours as an expression rather than in seconds
@@ -15,18 +16,35 @@ fn main() {
 
     // immutability - everything else should be plain let 
 
-    let mut user_number = String::new();
+    const SECONDS_PER_MINUTE: u32 = 60;
+    const SECONDS_PER_HOUR: u32 = 60 * SECONDS_PER_MINUTE;
+    
 
-    println!("Please enter a number: ");
-    io::stdin()
-    .read_line(&mut user_number) {
-        Ok(n) => {
-            println!("You entered: {}", user_number);
-        }
-        Err(error) => {
-            println!("Error reading input: {}", error);
-        }
+    loop {
+
+        let mut user_number = String::new(); 
+
+        println!("Please enter a number: ");
+
+        io::stdin()
+            .read_line(&mut user_number)
+            .expect("Failed to read line");
+
+        println!("Your number is: {}", user_number.trim());
+
+        if user_number.trim() == "q" {
+            break;
+        };
+
+        let in_hours: u32 = user_number.trim().parse::<u32>().unwrap() / SECONDS_PER_HOUR;
+        println!("Your number in hours is: {}", in_hours);
+
+        let in_minutes: u32 = user_number.trim().parse::<u32>().unwrap() % SECONDS_PER_HOUR / SECONDS_PER_MINUTE;
+        println!("Your number in minutes is: {}", in_minutes);
+
+        let in_seconds: u32 = user_number.trim().parse::<u32>().unwrap() % SECONDS_PER_MINUTE;
+        println!("Your number in seconds is: {}", in_seconds);
+
     }
-    .expect("Failed to read line");
 
 }
